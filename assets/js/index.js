4eae672de7c0942813f35fefed69e78476ab32c4
Gedeón Studios <123525973+GedeonStudios@users.noreply.github.com>
2023-04-28 09:42:47 -0400

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,47 @@
-const header_menu = document.querySelector('.header-menu'),
-  not_loaded_class = 'not-loaded',
-  reloj = document.querySelector('.present_relej'),
-  home = document.querySelector('#home'),
-  lde = document.querySelector('.loader'),
-  hi = document.querySelector('.home_intro'),
-  ht = document.querySelector('.home_text'),
-  ct = document.querySelector('.page-content');
-var scrollbar = window.Scrollbar
+const header_menu = document.querySelector(".header-menu"),
+  not_loaded_class = "not-loaded",
+  reloj = document.querySelector(".present_relej"),
+  home = document.querySelector("#home"),
+  lde = document.querySelector(".loader"),
+  hi = document.querySelector(".home_intro"),
+  ht = document.querySelector(".home_text"),
+  ct = document.querySelector(".page-content");
+var scrollbar = window.Scrollbar;
 
 function is_touch_enabled() {
-  return ('ontouchstart' in window) ||
-    (navigator.maxTouchPoints > 0) ||
-    (navigator.msMaxTouchPoints > 0);
+  return (
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
 }
-
-
-
 
 function LoadMianPage(LoadElemt, IntroFrame, ContentElemt) {
   if (!LoadElemt) {
-    return console.error('Parámetros no válidos.');
+    return console.error("Parámetros no válidos.");
   }
 
-  LoadElemt.classList.add('loaded');
+  LoadElemt.classList.add("loaded");
 
   setTimeout(() => {
     if (IntroFrame && ContentElemt) {
-      IntroFrame.classList.add('intro');
-      LoadElemt.style.display = 'none';
-      ContentElemt.style.display = 'block';
+      IntroFrame.classList.add("intro");
+      LoadElemt.style.display = "none";
+      ContentElemt.style.display = "block";
     }
 
     setTimeout(() => {
       if (!header_menu.classList.contains(not_loaded_class)) {
-        console.error(`El elemento ${header_menu} no tiene la clase de pre-carga: ${not_loaded_class}`);
+        console.error(
+          `El elemento ${header_menu} no tiene la clase de pre-carga: ${not_loaded_class}`
+        );
       } else {
         header_menu.classList.remove(not_loaded_class);
-        ht.classList.add('fadeAnim')
+        ht.classList.add("fadeAnim");
       }
-    }, 500)
+    }, 500);
   }, 500);
 }
-
 
 function GaspAnimtionLoad(scroll, istouch) {
   const elements = {
@@ -69,6 +69,7 @@ function GaspAnimtionLoad(scroll, istouch) {
     elements.createCoins.classList.add("InAnim");
 
     if (isHeightThan(999) && isScrollThan(1000)) {
+      console.log(scroll.y, '+900');
       elements.createFooter.classList.add("fadeAnim");
       elements.textsFlex.classList.add("fadeAnim");
 
@@ -82,6 +83,7 @@ function GaspAnimtionLoad(scroll, istouch) {
         }
       }
     } else if (isScrollThan(1050) && isHeightThan(600) && !isHeightThan(700)) {
+      console.log(scroll.y, '-700');
       elements.textsFlex.classList.add("fadeAnim");
       elements.createFooter.classList.add("fadeAnim");
       if (isScrollThan(1600)) {
@@ -95,6 +97,7 @@ function GaspAnimtionLoad(scroll, istouch) {
       }
       
     } else if (isScrollThan(760) && !isHeightThan(900) && isHeightThan(780)) {
+      console.log(scroll.y, '+780');
       elements.createFooter.classList.add("fadeAnim");
       elements.textsFlex.classList.add("fadeAnim");
 
@@ -108,6 +111,7 @@ function GaspAnimtionLoad(scroll, istouch) {
         }
       }
     } else if (isScrollThan(1100) && isHeightThan(700) && !isHeightThan(760)) {
+      console.log(scroll.y, '+700');
       elements.easyText.classList.add("fadeAnim");
 
       if (isScrollThan(2479) && !isScrollThan(3090)) {
@@ -116,6 +120,7 @@ function GaspAnimtionLoad(scroll, istouch) {
         header_menu.classList.remove("black");
       }
     } else if (isScrollThan(1050) && isHeightThan(900) && !isHeightThan(999)) {
+      console.log(scroll.y, '+900');
       elements.textsFlex.classList.add("fadeAnim");
       elements.createFooter.classList.add("fadeAnim");
       if (isScrollThan(1600)) {
@@ -134,34 +139,32 @@ function GaspAnimtionLoad(scroll, istouch) {
   }
 }
 
-
 function hover(element, backgroundElement, className) {
   if (element) {
-    element.addEventListener('mouseenter', () => {
+    element.addEventListener("mouseenter", () => {
       backgroundElement.classList.add(className);
     });
 
-    element.addEventListener('mouseleave', () => {
+    element.addEventListener("mouseleave", () => {
       backgroundElement.classList.remove(className);
     });
   }
 }
 
-
-hover(reloj, home, 'cls');
+hover(reloj, home, "cls");
 
 setInterval(() => {
-  const dayElement = document.querySelector('.d');
-  const hourElement = document.querySelector('.h');
-  const minuteElement = document.querySelector('.m');
-  const secondElement = document.querySelector('.s');
+  const dayElement = document.querySelector(".d");
+  const hourElement = document.querySelector(".h");
+  const minuteElement = document.querySelector(".m");
+  const secondElement = document.querySelector(".s");
   const currentDate = new Date();
-  const targetDate = new Date('2023 july 01 00:00:00 GMT-0400');
+  const targetDate = new Date("2023 july 01 00:00:00 GMT-0400");
 
   const diffInMilliseconds = Math.floor(targetDate - currentDate);
   const diffInSeconds = Math.floor(diffInMilliseconds / 1000);
   const days = Math.floor(diffInSeconds / (60 * 60 * 24));
-  const hours = Math.floor(diffInSeconds / (60 * 60) % 24);
+  const hours = Math.floor((diffInSeconds / (60 * 60)) % 24);
   const minutes = Math.floor(diffInSeconds / 60) % 60;
   const seconds = Math.floor(diffInSeconds) % 60;
 
@@ -171,10 +174,8 @@ setInterval(() => {
   secondElement.innerText = seconds;
 }, 1000);
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  LoadMianPage(lde, hi, ct)
+document.addEventListener("DOMContentLoaded", () => {
+  LoadMianPage(lde, hi, ct);
 
   scrollbar = Scrollbar.init(home, {
     alwaysShowTracks: true,
@@ -182,13 +183,12 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   if (is_touch_enabled()) {
-    document.querySelector('body').classList.add('IsTouch')
-    document.querySelector('.page-content').classList.add('IsTouch')
-    document.querySelector('.page-wrapper').classList.add('IsTouch')
+    document.querySelector("body").classList.add("IsTouch");
+    document.querySelector(".page-content").classList.add("IsTouch");
+    document.querySelector(".page-wrapper").classList.add("IsTouch");
   }
 
   scrollbar.addListener(({ offset }) => {
-    GaspAnimtionLoad(offset, is_touch_enabled())
+    GaspAnimtionLoad(offset, is_touch_enabled());
   });
 });
-
